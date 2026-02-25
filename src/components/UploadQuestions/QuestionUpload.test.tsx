a7c8d7d965a0a82952f673f0e5ceb760b8b85d00
Dave Schinkel <dschinkel@gmail.com>
2026-02-25 11:47:16 -0600

    refactor: store .csv for test in the root test folder

--- src/components/UploadQuestions/QuestionUpload.test.tsx
+++ src/components/UploadQuestions/QuestionUpload.test.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { UploadQuestions } from './UploadQuestions.tsx';
+import * as fs from 'fs';
+import * as path from 'path';
 
 describe('Upload Questions', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  test('shows upload questions', () => {
+  test('shows uploaded questions', () => {
     render(<UploadQuestions onUpload={jest.fn()} />);
     expect(screen.getByLabelText(/upload csv/i)).toBeInTheDocument();
   });
 
-  test('calls onUpload prop when file is selected', () => {
+  test('initiates the upload', () => {
     const mockOnUpload = jest.fn();
     render(<UploadQuestions onUpload={mockOnUpload} />);
-    const file = new File(['Question 1'], 'test.csv', { type: 'text/csv' });
+    
+    const csvContent = fs.readFileSync(path.resolve(__dirname, '../../../test/questions.csv'), 'utf-8');
+    const file = new File([csvContent], 'questions.csv', { type: 'text/csv' });
     const input = screen.getByLabelText(/upload csv/i);
 
     fireEvent.change(input, { target: { files: [file] } });
