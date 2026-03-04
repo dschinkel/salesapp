ddb35a4e602828676cc3995577805dfd5f6283cf
Dave Schinkel <dschinkel@gmail.com>
2026-03-03 22:06:52 -0600

    feat: add lint rule to prevent TS classes

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,18 @@ export default [
         ecmaFeatures: { jsx: true },
       },
     },
-    rules: {},
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'ClassDeclaration',
+          message: 'Classes are forbidden. Use functional modules with plain functions and objects.',
+        },
+        {
+          selector: 'ClassExpression',
+          message: 'Classes are forbidden. Use functional modules with plain functions and objects.',
+        },
+      ],
+    },
   },
 ];
